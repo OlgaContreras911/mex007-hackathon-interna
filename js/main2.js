264d2a0f1ebd8d783fd428174d824ec355e4d2c4
OlgaContreras911 <rockersita.911@gmail.com>
2019-03-01 13:06:52 -0600

    versión para presentar

--- js/main2.js
+++ js/main2.js
@@ -31,8 +31,7 @@ const printMovies = (movieData) => {
      <h2 class="text-center text-uppercase text-secondary mb-0">${element.Title.toUpperCase()}</h2>
      <div class="row">
        <div class="col-md-6 col-lg-4">
-         <a class="portfolio-item d-block mx-auto" href="#portfolio-modal-1">
-           <div class="portfolio-item-caption d-flex position-absolute h-100 w-100">
+            <div class="portfolio-item-caption d-flex position-absolute h-100 w-100">
              <div class="portfolio-item-caption-content my-auto w-100 text-center text-white">
              </div>
            </div>
@@ -41,7 +40,6 @@ const printMovies = (movieData) => {
         <p>Plot: ${element.Plot}</p>
         <p>Actors: ${element.Actors}</p>
         <p>Country: ${element.Country}</p>
-         </a>
        </div>
      </div>
    </div>`;
